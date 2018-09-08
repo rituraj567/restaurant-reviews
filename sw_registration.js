ac56217e85af1cad69622138c2bec649a9d50c14
Ritu <ritu@gmail.com>
2018-09-08 20:28:03 +0530

    first commit

---
+++ sw_registration.js
@@ -0,0 +1,15 @@
+
+    /**
+     *     Service Worker registration
+     */
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/sw.js', {scope: "/"})
+    .then(reg => {
+      console.log('Service Worker Registration Successful: ' + reg.scope);
+    })
+    .catch(error => {
+      console.log('Service Worker Registration Failed: ' + error);
+    });
+}
